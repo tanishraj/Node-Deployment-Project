2930d247055f963b8a353dfc393d61d8377aa679
tanishraj <tanishraj.quitegrowth@gmail.com>
2019-06-01 12:55:46 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+// Setting up node for partial files / Code
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Setting up the view engine
+app.set('view engine', 'hbs');
+
+// Server setup to serve the folder
+app.use(express.static(__dirname + '/public'));
+
+// Maintenance Page
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// })
+
+// Register a middleware
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ? ${req.url}`;
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log("Unable to append to server.log");
+		}
+	});
+	next();
+});
+
+// HBS helpers to reuse attributes on template
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+})
+
+// Capitalize helper HBS
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		title: 'Home Page',
+		heading: 'Welcome to Home Page'
+	});
+})
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		title: 'About Page',
+		heading: 'This is About Page'
+	});
+})
+
+app.listen(8080, () => {
+	console.log(
+		'Server is up and will serve you on http://localhost:8080/'
+	);
+});
